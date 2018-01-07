6f90ad92127bd711af1987e1e7585f64c50bc686
Ramin <Ramin@PC_170619>
2018-01-07 18:21:48 +0400

    ms Acces Connection to Password Crypted DB with password

---
+++ Test/src/test/CryptCodecOpener.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import net.ucanaccess.jdbc.JackcessOpenerInterf­ace;
+import com.healthmarketscience.jackcess.CryptCo­decProvider;
+import com.healthmarketscience.jackcess.Databas­e;
+import com.healthmarketscience.jackcess.Databas­eBuilder;
+
+public class CryptCodecOpener implements JackcessOpenerInterface {
+
+    public Database open(File fl, String pwd) throws IOException {
+        DatabaseBuilder dbd = new DatabaseBuilder(fl);
+        dbd.setAutoSync(false);
+        dbd.setCodecProvider(new CryptCodecProvider(pwd));
+        dbd.setReadOnly(false);
+        return dbd.open();
+    }
+}
---
+++ Test/src/test/Test.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Ramin
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Connection conn;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:ucanaccess://test.accdb;jackcessOpener=test.CryptCodecOpener", "hamreen", "12345");
+            System.out.println("succes");
+
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery("select * from user");
+
+            System.out.println(rs.next());
+            System.out.println(rs.getString(1));
+            System.out.println(rs.getString(2));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
